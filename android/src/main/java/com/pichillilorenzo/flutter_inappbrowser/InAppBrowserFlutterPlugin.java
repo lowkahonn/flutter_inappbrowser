a5b91ad4973d2b305f866d512f2f8cd5570edfe7
Low Kah Onn <lowkahonn0106@gmail.com>
2019-12-31 16:13:58 +0800

    jsonobject to string

--- android/src/main/java/com/pichillilorenzo/flutter_inappbrowser/InAppBrowserFlutterPlugin.java
+++ android/src/main/java/com/pichillilorenzo/flutter_inappbrowser/InAppBrowserFlutterPlugin.java
@@ -311,7 +311,7 @@ public class InAppBrowserFlutterPlugin implements MethodCallHandler {
         result.success(getCopyBackForwardList(uuid));
         break;
       case "loadPaymentData":
-        Map paymentDataRequest = call.argument("paymentDataRequest");
+        JSONObject paymentDataRequest = new JSONObject((Map) call.argument("paymentDataRequest"));
         PaymentDataRequest request = PaymentDataRequest.fromJson(paymentDataRequest.toString());
         int env = WalletConstants.ENVIRONMENT_PRODUCTION;
         PaymentsClient client = Wallet.getPaymentsClient(activity,
